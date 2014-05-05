f9784574cf43a79bca09e0d2c55d4f2427261bb9
Forrest Sutton <forrestsutton@gmail.com>
2014-05-05 19:56:53 -0400

    trying to get the templates to work

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+import os
+import jinja2
+import urllib
 import webapp2
-from views import MainPage
+
+from google.appengine.ext ndb
 
 
-#jinja_environment = jinja2.Environment(
-#    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+ "/templates"),
-#    extensions=['jinja2.ext.autoescape'], 
-#    autoescape=True)
-#
-#class MainPage(webapp2.RequestHandler):
-#   def get(self):
-#       template_values = {
-#                          'welcome': 'welcome to the page'
-#
-#       }	
-#       template = jinja_environment.get_template('index.html')
-#       self.response.out.write(template.render(template_values))
-#
-#
-#
-app = webapp2.WSGIApplication([('/', MainPage)],
-			       debug=True)
+jinja_environment = jinja2.Environment(
+   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+ "/templates"),
+   extensions=['jinja2.ext.autoescape'],  autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+      template_values = {
+                         'welcome': 'welcome to the page'
+       }	
+      template = jinja_environment.get_template('index.html')
+      self.response.out.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
