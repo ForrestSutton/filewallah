f4614a8fd80056b00a69e603f3665ffb9d0c4a63
Forrest Sutton <forrestsutton@gmail.com>
2014-05-07 21:04:46 -0400

    adding driveproperties list

--- driveprop.js
+++ driveprop.js
@@ -1,4 +1,4 @@
-driveproperties
+// driveproperties
 
 {
   "kind": "drive#property",
@@ -8,3 +8,13 @@ driveproperties
   "visibility": string,
   "value": string
 }
+
+
+// the model:
+
+// 		drivePoperties
+//         linked_to_comments (searchable)
+//         only shared 
+//         users - are the user able to view this file ?
+//   https://developers.google.com/drive/v2/reference/permissions#resource
+// https://developers.google.com/drive/v2/reference/permissions/list
